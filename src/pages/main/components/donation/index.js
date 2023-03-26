1f6caf293e4ae5f08090c6dd82e33f01d742b88d
olyalass <olyalass@icloud.com>
2023-03-26 16:44:36 +0200

    feat: create donation block

---
+++ src/pages/main/components/donation/index.js
@@ -0,0 +1,49 @@
+import "./style.scss";
+
+export class Donation {
+  item;
+
+  constructor() {
+    this.item = document.createElement("div");
+    this.item.classList.add("donation");
+
+    const donContainer = document.createElement("div");
+    donContainer.classList.add("donation__container");
+    this.item.appendChild(donContainer);
+
+    const donImg = document.createElement("img");
+    donImg.classList.add("donation__img");
+    donImg.setAttribute(
+      "src",
+      require("../../../../../assets/images/donation-dog.png")
+    );
+    const donTextBlock = document.createElement("div");
+    donTextBlock.classList.add("donation__text-block");
+    donContainer.append(donImg, donTextBlock);
+
+    const donTitle = document.createElement("h3");
+    donTitle.classList.add("donation__title");
+    donTitle.textContent = "You can also make a donation";
+    const donSubtitle = document.createElement("p");
+    donSubtitle.classList.add("donation__subtitle");
+    donSubtitle.textContent = "Name of the bank / Type of bank account";
+    const donCard = document.createElement("div");
+    donCard.classList.add("donation__card");
+    const donText = document.createElement("p");
+    donText.classList.add("donation__text");
+    donText.textContent =
+      "Legal information and lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a ipsum at libero sagittis dignissim sed ac diam. Praesent ultrices maximus tortor et vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus.";
+    donTextBlock.append(donTitle, donSubtitle, donCard, donText);
+
+    const donCardImg = document.createElement("img");
+    donCardImg.classList.add("donation__card__img");
+    donCardImg.setAttribute(
+      "src",
+      require("../../../../../assets/icons/credit-card.svg")
+    );
+    const donCardNumber = document.createElement("p");
+    donCardNumber.classList.add("donation__card__number");
+    donCardNumber.textContent = "8380 2880 8028 8791 7435";
+    donCard.append(donCardImg, donCardNumber);
+  }
+}
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -4,11 +4,20 @@ import { Footer } from "../../shared_components/footer";
 import { About } from "./components/about";
 import { Friends } from "./components/friends";
 import { Help } from "./components/help";
+import { Donation } from "./components/donation";
 
 const body = document.querySelector("body");
 const header = new Header(false, "About");
 const about = new About();
 const friends = new Friends();
 const help = new Help();
+const donation = new Donation();
 const footer = new Footer();
-body.append(header.item, about.item, friends.item, help.container, footer.item);
+body.append(
+  header.item,
+  about.item,
+  friends.item,
+  help.container,
+  donation.item,
+  footer.item
+);
